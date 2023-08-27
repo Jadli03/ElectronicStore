4b53f259799775aeb34a458826e9d447e16ebd94
Suraj Jadli <surajjadli@yahoo.com>
2023-08-27 19:27:45 +0530

    method argument not valid handler

--- src/main/java/com/spring/eStore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/eStore/exception/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +27,18 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .success(true).build();
         return new ResponseEntity<>(res,HttpStatus.NOT_FOUND);
+    }
+
+    // methodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response = new HashMap<>();
+        allErrors.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError)objectError).getField();
+            response.put(field,message);
+        });
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 }
